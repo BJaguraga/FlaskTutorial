42c0ca8079e5fcb312e8f53b99c605dcaf2f66cc
Bjaguraga <bjaguraga1>
2016-07-06 01:31:56 +0200

    ap-0001: Stating app, int and float another route added

--- app.py
+++ app.py
@@ -3,19 +3,22 @@ app = Flask(__name__)
 
 ''' adding support for int and float '''
 
+try:
 
-@app.route('/<int:name>')
-def index(name):
-    if name == 10:
-        return 'Salam welcome . Your lucky number is  %s'%name
-    return 'hello salam alaikum  %s' %name
-
-
-@app.route('/<float:post_id>')
-def float_num(post_id):
-    return 'Float is {}'.format(post_id)
+    @app.route('/getint/<int:name>')
+    def index(name):
+        if name == 10:
+            return 'Salam welcome . Your lucky number is  %s'%name
+        return 'hello salam alaikum  %s' %name
 
 
-if __name__ == '__main__':
-    app.debug = True
+    @app.route('/getfloat/<float:post_id>')
+    def float_num(post_id):
+        return 'Float is {}'.format(post_id)
+
+
+    if __name__ == '__main__':
+        app.debug = True
     app.run()
+except ValueError:
+    print('error in app')
