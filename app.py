c510317809fdadb24063193d055549a68be1adbd
Bjaguraga <bjaguraga1>
2016-07-06 01:21:11 +0200

    ap-0001: Stating app, int and float support tested

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, redirect, request, render_template
 app = Flask(__name__)
 
-@app.route('/<name>')
+''' adding support for int and float '''
+
+
+@app.route('/<int:name>')
 def index(name):
-    if name == 'Ousman':
-        return 'Salam %s'%name
-    return 'hello salam alaikum edi mubarak just %s' %name
+    if name == 10:
+        return 'Salam welcome . Your lucky number is  %s'%name
+    return 'hello salam alaikum  %s' %name
+
+
+@app.route('/<float:post_id>')
+def float_num(post_id):
+    return 'Float is {}'.format(post_id)
 
 if __name__ == '__main__':
     app.debug = True
